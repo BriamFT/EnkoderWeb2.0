f5ede93546ccc4f4fa3a861f992bc64821f9f1ff
Luis Cisneros Torres <luis.cisneros@enkoder.com.mx>
2021-10-04 23:00:43 -0500

    Cambios en navbar, se agrega logo de empresa.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,6 @@
 <script>
+	import enkoder from "../assets/images/enkoder.png";
+
 	export let segment;
 	// export const elements = [
 	// 	{endpoint: 'enkoder', label: 'Enkoder' },
@@ -37,22 +39,37 @@
 	[aria-current]::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
+		width: calc(100% - 2.3em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: #FE8104;
 		display: block;
 		bottom: -1px;
 	}
 
 	a {
+		font-weight: 450;
 		text-decoration: none;
-		padding: 1em 0.5em;
+		text-align: center;
+		padding: 15px 18px;
 		display: block;
+	}
+
+	a:hover {
+		color: #FE8104;
+	}
+	
+	.img-div {
+		padding: 15px 25px;
+	}
+
+	img:hover {
+		cursor: pointer;
 	}
 </style>
 
 <nav>
 	<ul>
+		<li><div class="img-div"><img alt="Enkoder" src="{enkoder}" height="40px"></div></li>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">About us</a></li>
 		<!-- {#each elements as element}
