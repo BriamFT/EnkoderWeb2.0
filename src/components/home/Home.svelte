72078e6e330b563b85474ead3514265aaa1b7643
Luis Cisneros Torres <luis.cisneros@enkoder.com.mx>
2021-10-05 10:25:30 -0500

    Modularización del index y home

---
+++ src/components/home/Home.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Fold from "./Fold.svelte";
+</script>
+
+<style>
+	/*h1, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	/*figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}*/
+</style>
+
+<Fold />
